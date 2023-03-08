9d32d179fb80a02feb454ff36f4e4b606d16728f
kairat <batyrbekov.06@mail.ru>
2023-03-08 21:33:48 +0600

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,48 @@
+import icon from '../assets/Group.png'
+
+
 const Footer = (Props) => {
     return (
         <footer>
+
+            <div className='footer_content'>
+
+
+            <div className='footer_icon'>
+           <img src={icon} alt="" />
+            </div>
+
+<p className='new_collection'>подписаться на обновления:</p>
+<input type="text"  placeholder='введите e-mail'/> <button>подписаться</button>
+
+<p className='footer_reg'>регистрируясь вы соглашаетесь с нашими условиями</p>
+
+<div className='terms'>
+    <ul>
+        <li>доставка</li>
+        <li>оплата</li>
+        <li>контакты</li>
+    </ul>
+</div>
+
+<div className='terms_2'>
+
+    <ul>
+        <li>отзывы</li>
+        <li>сертификаты</li>
+        <li>каталог</li>
+    </ul>
+
+</div>
+<div className="line"></div>
+
+            </div>
+
+
+
+
+
+
         </footer>
     );
 };
